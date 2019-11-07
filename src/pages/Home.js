f3f33c15278b8a4961857cf3667dbe463da30c2a
milkyeway <52460221+milkyeway@users.noreply.github.com>
2019-11-07 13:52:13 +0800

    Add files via upload

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,25 @@
 import React from 'react'
 // import { Link } from 'react-router-dom'
 // import PathNow from '../components/PathNow'
-import TodoApp from '../components/TodoApp'
+import Carousel from '../components/Home/Carousel'
+import Home_Container_LatestEvents from '../components/Home/Home_Container_LatestEvents'
+import Home_Container_LatestArticles from '../components/Home/Home_Container_LatestArticles'
+import Home_Wine from '../components/Home/Home_Wine'
+import Footer from '../components/Navigation_Navber/Footer'
+
+// Container_News
+
 // import Choosen from "../Chosen"
+
+
 const Home = () => {
     return (
         <>
-            {/* <Choosen> */}
-            <h1>Home</h1>
-            <TodoApp/>  
-            {/* </Choosen> */}
-
+            <Carousel />
+            <Home_Container_LatestEvents />
+            <Home_Container_LatestArticles />
+            <Home_Wine/>
+            <Footer />
         </>
     )
 }
